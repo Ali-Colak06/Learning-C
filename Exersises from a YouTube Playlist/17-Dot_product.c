0546edf9eda37f8d2b5647286424949fb6000f7b
Ali Çolak <ali.colak25@marun.edu.tr>
2025-10-14 11:46:31 +0300

    17-Dot_product.c added

---
+++ Exersises from a YouTube Playlist/17-Dot_product.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+// Vector Addition
+
+float dot_product(float vector1[], float vector2[]);
+
+int main(){
+    float v1[] = {1,2,3};
+    float v2[] = {3,4,5};
+    float result;
+    result = dot_product(v1, v2);
+    printf("Result: %f\n", result);
+
+    return 0;
+}
+
+float dot_product(float vector1[], float vector2[]){
+    float sum = 0;
+    for (int i = 0; i<3; i++){
+        sum = sum + (vector1[i]*vector2[i]);
+    }
+    return sum;
+}
